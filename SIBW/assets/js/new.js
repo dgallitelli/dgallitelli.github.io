b3e8febbd9fe0cd409f4d6199c0c355a947f108e
dave95 <davidegallitelli@gmail.com>
2016-03-17 19:10:58 +0100

    Fixed link, updated SIBW project

--- SIBW/assets/js/new.js
+++ SIBW/assets/js/new.js
@@ -9,3 +9,21 @@ function changeZoomPicture(newPic) {
 function highlightBG(paramId) {
     document.getElementById(paramId).css.background = "#000";
 }
+
+function today() {
+    var day = new Date();
+    var dd = day.getDate();
+    var mm = day.getMonth()+1; //January is 0!
+    var yyyy = day.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd
+    } 
+
+    if(mm < 10) {
+        mm='0'+mm
+    } 
+
+    day = yyyy+'-'+mm+'-'+dd;
+    document.getElementById("input-llegada").min=day;
+}
