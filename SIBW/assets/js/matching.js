9114e1b5f4524cda3387513b8395e8e372f53536
Davide Gallitelli <davidegallitelli@gmail.com>
2016-04-03 18:04:33 +0200

    Updating SIBW
    Updated SIBW to last rev

--- SIBW/assets/js/matching.js
+++ SIBW/assets/js/matching.js
@@ -1,16 +1,19 @@
-(function() {
-    /* matchHeight example */
-    $(function() {
-        // apply your matchHeight on DOM ready (they will be automatically re-applied on load or resize)
-        // get test settings
-        var byRow = $('section').hasClass('content');
-
-        // apply matchHeight to each item container's items
-        $('.container').each(function() {
-            $(this).children('.one-of-three-col').matchHeight({
-                byRow: byRow
-            });
-        });
-    });
-
-})();
+function matchHeight() {
+    var myContainer = document.getElementById('3col-container'), i = 0, j = 1, maxHeight = 0, element = 0, childCount = myContainer.childElementCount, children = myContainer.childNodes;
+    
+    //alert('Children number: '+childCount);
+    
+    for (i = 0; i < childCount; i++) {
+        if (children[j].clientHeight > maxHeight) {
+            maxHeight = children[j].clientHeight;
+        }
+        //alert('Current height: '+children[j].clientHeight+'; current maxHeight: '+maxHeight);
+        j += 2;
+    }
+    //alert('Setting heights');
+    j = 1;
+    for (i = 0; i < childCount; i++) {
+        children[j].style.height = maxHeight + 'px';
+        j += 2;
+    }
+}
