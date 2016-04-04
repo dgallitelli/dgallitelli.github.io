525af2a647a4b0a8bd9b76ae91d3404b5b86d1f6
Davide Gallitelli <davidegallitelli@gmail.com>
2016-04-04 14:44:02 +0200

    Updating to last rev
    With (mostly) working slider and some modifications.

--- SIBW/assets/js/new.js
+++ SIBW/assets/js/new.js
@@ -57,13 +57,53 @@ function validateEmail(email, idElement) {
     else formEmail.style.color = "green";
 }
 
+function sliderContent(valor){
+    var didascalia = document.getElementById("prom-slider-text");
+    document.getElementById("promSlider").src = "images/fotos/slider/" + valor + ".jpg";
+    switch(valor){
+        case 2:
+            didascalia.innerHTML = "<h3><strong>DOS NOCHES</strong></h3> Disfute de un 10% de descuento en estancias de un minimo de dos noches.";
+            break;
+        case 1:
+            didascalia.innerHTML = "<h3><strong>10% DE DESCUENTO</strong></h3> Disfute de un 10% de descuento con esta tarifa no reembolsable."
+            break;
+        case 3:
+            didascalia.innerHTML = "<h3><strong>RESERVA ANTICIPADA</strong></h3> Disfute de un 10% de descuento reservando con 21 dias de antelacion.";
+            break;
+        case 4:
+            didascalia.innerHTML = "<h3><strong>HABITACION DOBLE + TREN TURISTICO</strong></h3> Dos noches en habitacion estandar, desayuno continental, dos tickets para uso del Tren Turistico de Granada.";
+            break;
+        case 5:
+            didascalia.innerHTML = "<h3><strong>HABITACION DOBLE + BAÑO ARABE</strong></h3> Dos noches en habitacion estandar, desayuno continental, session de baño arabe en Hamman Al Andalus Granada.";
+            break;
+        case 6:
+            didascalia.innerHTML = "<h3><strong>HABITACION DOBLE + ESPECTACULO DE FLAMENCO</strong></h3> Dos noches en habitacion estandar, desayuno continental, espectaculo de flamenco en \"El templo del Flamenco\".";
+            break;
+        case 7:
+            didascalia.innerHTML = "<h3><strong>HABITACION DOBLE + VISITA GUIADA ALHAMBRA</strong></h3> Dos noches en habitacion estandar, desayuno continental, visita guiada a la Alhambra.";
+            break;
+        case 8:
+            didascalia.innerHTML = "<h3><strong>HABITACION DOBLE + TREN TURISTICO + ESPECTACULO DE FLAMENCO</strong></h3> Disfruta de la oferta por el tren turistico y por el espectaculo de flamenco en un solo pack!.";
+            break;
+    }
+}
 
 function updateSlider(sentido) {
-    var numeroProm = 8;
+    var numeroProm = 9;
     var imageSrc = document.getElementById("promSlider").src;
     var temp = imageSrc.split("images/fotos/slider/");
     var myValue = parseInt(temp[temp.length-1].split("."));
-    if (sentido == "next") myValue = (myValue+1) % numeroProm;
-    else myValue= (myValue-1) % numeroProm;
-    document.getElementById("promSlider").src = "images/fotos/slider" + myValue + ".jpg";
+    if (sentido == "next") {
+        myValue = Math.abs(myValue+1) % numeroProm;
+        if (myValue == 0) myValue = 1;
+    }
+    else {
+        myValue= Math.abs(myValue-1) % numeroProm;
+        if (myValue == 0) myValue = numeroProm-1;
+    }
+    sliderContent(myValue);
+}
+
+window.onload = function(){
+    setInterval(updateSlider('next'), 5000);
 }
